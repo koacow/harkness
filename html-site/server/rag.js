f6112b793bc050ce9b886b17545297083bc62d20
koacow <khoa.cnd@gmail.com>
2024-11-25 14:57:09 -0500

    removed html site and cleaned up repo

--- html-site/server/rag.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const RAGController = require('../../rag/api/routes/ragController');
-
-router.post('/query', RAGController.handleQuery);
-router.post('/upload', RAGController.handleMaterialUpload);
-
-module.exports = router;
