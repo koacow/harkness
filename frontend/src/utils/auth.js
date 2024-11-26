ff2b50e4483eb20a731bd7c34a8ead61cb4f4d50
koacow <khoa.cnd@gmail.com>
2024-11-26 12:48:44 -0500

    added auth function descriptions

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,6 +1,13 @@
 import supabase from "./supabase";
 
-// TODO: Implement signUpNewUser function with more options
+/**
+ * Function to sign up a new user
+ * @param {string} email - The user's email address
+ * @param {string} password - The user's password
+ * @param {string} firstName - The user's first name
+ * @param {string} lastName - The user's last name
+ * @returns {Promise} - The user's data or null if an error occurred 
+ */
 export const signUpNewUser = async (email, password) => {
     const { data, error } = await supabase.auth.signUp({
         email: 'example@email.com',
@@ -11,7 +18,12 @@ export const signUpNewUser = async (email, password) => {
     })
 }
 
-// TODO: Implement signInWithEmail function
+/**
+ * Function to sign in a user with email and password
+ * @param {string} email - The user's email address
+ * @param {string} password - The user's password
+ * @returns {Promise} - The user's data or null if an error occurred 
+ */
 export const signInWithEmail = async (email, password) => {
     const { data, error } = await supabase.auth.signInWithPassword({
         email,
